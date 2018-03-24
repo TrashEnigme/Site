eb687516e54a2c875bd89acbf7a26662fca652e7
TrashEnigme <37696994+TrashEnigme@users.noreply.github.com>
2018-03-24 18:21:57 +0100

    Add files via upload

---
+++ bonjour.js
@@ -0,0 +1,13 @@
+function Login(){
+	var done=0;
+	var username=document.login.username.value;
+	var password=document.login.password.value;
+
+	if (username=="Trash" && password=="Myst") {
+		window.location="site.html";
+	}
+
+	if (username=="KyaBoomy" && password=="EhRegardePasLeCodeSTP") { 
+		window.location="site.html";
+	}
+}
